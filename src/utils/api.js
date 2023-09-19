f85828b76545d76a405fed4f779d576111e53c9d
mac1 <mac1@MacBookPro.local>
2023-09-19 10:49:06 +0500

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": "18b2f977d6mshdc51b422855237cp17e994jsn726438b05943",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
